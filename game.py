Feature - Cleaned GAME class - Added fixed snake len - Added graf smoothing - Added average calculations to graf data

game.py
@@ -2,15 +2,14 @@ from settings import *
 import numpy as np
 
 
-class game:
+class GAME:
     def __init__(self):
         self.snake = np.zeros([s_game_amount, s_size[0] * s_size[1], 2])
         self.done = np.ones(s_game_amount, dtype=bool)
-        self.point = np.zeros(s_game_amount, dtype=bool)
 
         self.last_position = np.zeros([s_game_amount, 2])
         self.distance_score = s_distance_score
-        self.random_poit = s_start_random_point
+        self.add_len = s_start_len
 
         # all available spots
         self.all_spots = []
@@ -22,7 +21,6 @@ class game:
         # Clear old snake
         self.snake[snake_number, 0:] = 0
         self.done[snake_number] = False
-        self.point[snake_number] = False
 
         # spawn snake head
         head = np.random.randint(1, [s_size[0] + 1, s_size[1] + 1], size=2)
@@ -94,7 +92,7 @@ class game:
         return
 
     # movements
-    def move_snake(self, action, snake_number):
+    def move_snake(self, snake_number, action):
         snake = np.copy(self.snake[snake_number, 1:])
         snake_copy = np.copy(snake)
 
@@ -114,9 +112,8 @@ class game:
 
         # move snake
         if np.all(snake[0] == snake[1]):
-            done = True
+            self.done[snake_number] = True
         else:
-            done = False
             # move snake body
             for i in range(len(snake) - 1):
                 # save last position for adding snake
@@ -126,22 +123,21 @@ class game:
                 snake[i + 1] = snake_copy[i]
 
         self.snake[snake_number, 1:] = snake
-        return done
+        return
 
     # check collision
-    def check(self, snake_number, done):
+    def check(self, snake_number):
+        done = self.done[snake_number]
         point = False
         if done:
-            # mark if game is done
-            self.done[snake_number] = True
-            return point, done
+            return point
 
         # point = got the apple / done = dead
         snake = self.snake[snake_number]
-        snake = snake[:s_max_len + 2]
-        done = self.done[snake_number]
-
+        apple = snake[0]
         head = snake[1]
+        body = snake[2:]
+
         # Check walls
         if np.any(head > s_size[0]):
             done = True
@@ -149,39 +145,39 @@ class game:
             done = True
         else:
             # Check if snake hit itself
-            for i in range(len(snake) - 2):
-                if np.all(head == snake[i + 2]):
+            for i in range(len(body)):
+                if np.all(head == body[i]):
                     done = True
-                if np.all(snake[i] == 0):
+                    break
+                if np.all(body[i] == 0):
                     break
 
-        # check apple
-        if not done:
-            apple = snake[0]
-            if np.all(head == apple):
-                point = True
-                # add snake
-                for i in range(s_max_len + 2):
-                    if np.all(snake[i] == 0):
-                        snake[i] = self.last_position[snake_number]
-                        break
-                # spawn new apple
-                game.spawn_apple(self, snake_number)
+        # check point
+        if not done and np.all(head == apple):
+            point = True
 
-            # put this somewhere else you stupid little boy
-            elif np.random.rand() <= self.random_poit or np.count_nonzero(snake) < s_point_until:
-                for i in range(s_max_len + 2):
-                    if np.all(snake[i] == 0):
-                        snake[i] = self.last_position[snake_number]
-                        break
-
-        # save results
-        self.point[snake_number] = point
+        # save done
         self.done[snake_number] = done
-        return point, done
+        return point
+
+    # add snake
+    def add_snake(self, snake_number, point):
+        snake = self.snake[snake_number]
+        body = snake[2:]
+        # point or "random point"
+        if point or np.count_nonzero(body) < (self.add_len * 2):
+            # Add last position to snake body
+            for i in range(s_max_len):
+                if np.all(body[i] == 0):
+                    body[i] = self.last_position[snake_number]
+                    break
+            if point:
+                # spawn new apple
+                self.spawn_apple(snake_number)
+        return
 
     # calculate rewards
-    def reward_calculation(self, point, snake_number):
+    def reward_calculation(self, snake_number, point):
         done = self.done[snake_number]
         snake = self.snake[snake_number]
 

info.py
@@ -5,8 +5,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import time
+from typing import List
 
-class info:
+
+class INFO:
     def __init__(self, tf):
         self.score_limit = 0
         self.step_limit = 0
@@ -14,12 +16,31 @@ class info:
         self.avg_step = []
         self.last_score = 0
         self.last_step = 0
-        self.test_count = []
+        self.episodes = []
         self.tensorflow_setups(tf)
+
+        self.all_episodes = []
+        self.episodes_1 = []
+        self.episodes_2 = []
+        self.episodes_3 = []
+        self.episodes_4 = []
+
+        self.all_scores = []
+        self.scores_1 = []
+        self.scores_2 = []
+        self.scores_3 = []
+        self.scores_4 = []
+
+        self.all_steps = []
+        self.step_1 = []
+        self.step_2 = []
+        self.step_3 = []
+        self.step_4 = []
+
 
     def tensorflow_setups(self, tf):
         config = tf.compat.v1.ConfigProto()
-        config.gpu_options.per_process_gpu_memory_fraction = 0.9
+        config.gpu_options.per_process_gpu_memory_fraction = 1
         config.gpu_options.allow_growth = True
         session = tf.compat.v1.Session(config=config)
 
@@ -54,13 +75,13 @@ class info:
 
         # draw everything
         background = np.zeros((s_size[0], s_size[1], 3), dtype=np.uint8)
-        background[int(apple[0]-1), int(apple[1]-1)] = s_red
-        background[int(head[0]-1), int(head[1]-1)] = s_green
+        background[int(apple[0] - 1), int(apple[1] - 1)] = s_red
+        background[int(head[0] - 1), int(head[1] - 1)] = s_green
 
         for i in range(len(body)):
             if np.any(body[i] == 0):
                 break
-            background[int(body[i, 0]-1), int(body[i, 1]-1)] = 80
+            background[int(body[i, 0] - 1), int(body[i, 1] - 1)] = 80
 
         return background
 
@@ -74,25 +95,39 @@ class info:
 
         return
 
+    def smooth(scalars: List[float], weight: float) -> List[float]:
+        last = scalars[0]  # First value in the plot (first timestep)
+        smoothed = list()
+        for point in scalars:
+            smoothed_val = last * weight + (1 - weight) * point  # Calculate smoothed value
+            smoothed.append(smoothed_val)  # Save it
+            last = smoothed_val  # Anchor the last smoothed value
+
+        return smoothed
+
     # make an info graf
-    def print_graf(self, epsilon, e, random_poit):
+    def print_graf(self, epsilon, e, add_len):
         # limits for difficult increase
-        score_limit = round(self.score_limit, 2)
+        score_limit = round(self.score_limit)
         step_limit = round(self.step_limit)
-        random_poit = round(random_poit, 2)
+        add_len = round(add_len)
 
         # plt prints
         epsilon = round(epsilon, 3)
         plt.title(f'Epsilon {epsilon}', loc='right')
-        plt.title(f'Min steps: {step_limit} scores: {score_limit} random: {random_poit}', loc='left')
+        plt.title(f'Min steps: {step_limit} scores: {score_limit} len: {add_len}', loc='left')
         plt.xlabel(f'Episodes {e}   games: {s_game_amount}')
         plt.ylabel("Scores / Steps")
 
         plt.grid(True)
 
+        # Smooth the data
+        # smoothed_scores = info.smooth(self.all_scores, 0.5)
+        # smoothed_steps = info.smooth(self.all_steps, 0.5)
+
         # plot scores
-        plt.plot(self.test_count, self.avg_scores, label=f'Scores {round(self.last_score,2)}')
-        plt.plot(self.test_count, self.avg_step, label=f'Steps {round(self.last_step,2)}')
+        plt.plot(self.all_episodes, self.all_scores, label=f'Scores {round(self.last_score, 2)}')
+        plt.plot(self.all_episodes, self.all_steps, label=f'Steps {round(self.last_step, 2)}')
 
         # show
         plt.legend()
@@ -102,5 +137,161 @@ class info:
         plt.cla()
 
         return
+
+    # balance steps fix this
+    def balance_steps(self):
+        step = self.avg_step
+        step_1 = self.step_1
+        step_2 = self.step_2
+        step_3 = self.step_3
+        step_4 = self.step_4
+
+        e, step_4 = self.calculation(step_4, count=200)
+        step_4 = np.append(step_4, e)
+
+        e, step_3 = self.calculation(step_3, count=100)
+        step_4 = np.append(step_4, e)
+
+        e, step_2 = self.calculation(step_2, count=50)
+        step_3 = np.append(step_3, e)
+
+        e, step_1 = self.calculation(step_1, count=20)
+        step_2 = np.append(step_2, e)
+
+        e, step = self.calculation(step, count=10)
+        step_1 = np.append(step_1, e)
+
+        self.avg_step = step
+        self.step_1 = step_1
+        self.step_2 = step_2
+        self.step_3 = step_3
+        self.step_4 = step_4
+
+        self.all_steps = np.concatenate([step_4, step_3, step_2, step_1, step])
+
+    # balance scores fix this
+    def balance_scores(self):
+        scores = self.avg_scores
+        scores_1 = self.scores_1
+        scores_2 = self.scores_2
+        scores_3 = self.scores_3
+        scores_4 = self.scores_4
+
+        e, scores_4 = self.calculation(scores_4, count=200)
+        scores_4 = np.append(scores_4, e)
+
+        e, scores_3 = self.calculation(scores_3, count=100)
+        scores_4 = np.append(scores_4, e)
+
+        e, scores_2 = self.calculation(scores_2, count=50)
+        scores_3 = np.append(scores_3, e)
+
+        e, scores_1 = self.calculation(scores_1, count=20)
+        scores_2 = np.append(scores_2, e)
+
+        e, scores = self.calculation(scores, count=10)
+        scores_1 = np.append(scores_1, e)
+
+        self.avg_scores = scores
+        self.scores_1 = scores_1
+        self.scores_2 = scores_2
+        self.scores_3 = scores_3
+        self.scores_4 = scores_4
+
+        self.all_scores = np.concatenate([scores_4, scores_3, scores_2, scores_1, scores])
+
+    # balance episodes fix this
+    def balance_episodes(self):
+        episodes = self.episodes
+        episodes_1 = self.episodes_1
+        episodes_2 = self.episodes_2
+        episodes_3 = self.episodes_3
+        episodes_4 = self.episodes_4
+
+        e, episodes_4 = self.calculation(episodes_4, count=200)
+        episodes_4 = np.append(episodes_4, e)
+
+        e, episodes_3 = self.calculation(episodes_3, count=100)
+        episodes_4 = np.append(episodes_4, e)
+
+        e, episodes_2 = self.calculation(episodes_2, count=50)
+        episodes_3 = np.append(episodes_3, e)
+
+        e, episodes_1 = self.calculation(episodes_1, count=20)
+        episodes_2 = np.append(episodes_2, e)
+
+        e, episodes = self.calculation(episodes, count=10)
+        episodes_1 = np.append(episodes_1, e)
+
+        self.episodes = episodes
+        self.episodes_1 = episodes_1
+        self.episodes_2 = episodes_2
+        self.episodes_3 = episodes_3
+        self.episodes_4 = episodes_4
+
+        self.all_episodes = np.concatenate([episodes_4, episodes_3, episodes_2, episodes_1, episodes])
+        return
+
+    def calculation(self, episodes, count):
+        e = []
+        if len(episodes) >= count:
+            half = int(len(episodes) / 2)
+            for i in range(half):
+                k = i * 2
+                e_avg = (episodes[k] + episodes[k + 1]) / 2
+                e.append(e_avg)
+            episodes = []
+
+        return e, episodes
+
+    # def balance_episodes(self, num_episodes=100, count=10):
+    #     print(self.episodes)
+    #     for _ in range(num_episodes):
+    #         for i in range(4, -1, -1):  # Iterate over all episodes from index 4 to 0
+    #             e, self.episodes[i] = self.calculation(self.episodes[i], count)
+    #             if i > 0:  # Only extend if not the last episode
+    #                 self.episodes[i - 1].extend(e)
+    #             else:  # If it's the last episode, extend all_episodes
+    #                 self.episodes[i].extend(e)
+    #         # Extend all_episodes after each iteration of the outer loop
+    #         self.all_episodes.extend([])
+    #
+    #         print(self.episodes)
+    #
+    #     quit()
+    #
+    # def calculation(self, episodes, count):
+    #     e = []
+    #     if len(episodes) >= count:
+    #         half = len(episodes) // 2
+    #         for i in range(half):
+    #             e_avg = (episodes[i * 2] + episodes[i * 2 + 1]) / 2
+    #             e.append(e_avg)
+    #         episodes = e[:]  # Update episodes with the new episodes
+    #     print(e, episodes)
+    #     quit()
+    #     return e, episodes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

main.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import time
 
 
-class main:
+class MAIN:
     def __init__(self):
         # define state
         self.ori_state = np.zeros(s_state_size)
@@ -59,34 +59,36 @@ class main:
 
     # game states
     def game_states(self, snake_number, r_testing):
-        done = False
         # create state
-        state = main.create_state(game.snake[snake_number], done)
+        state = main.create_state(game.snake[snake_number], game.done[snake_number])
 
         # pick action
         action = DQNA.get_qs(state, r_testing)
 
         # move snake
-        done = game.move_snake(action, snake_number)
+        game.move_snake(snake_number, action)
 
         # check snake
-        point, done = game.check(snake_number, done)
+        point = game.check(snake_number)
+
+        # add snake
+        game.add_snake(snake_number, point)
 
         # reward calculations
-        step_reward = game.reward_calculation(point, snake_number)
+        step_reward = game.reward_calculation(snake_number, point)
 
         if r_testing:
             self.validation_reward += step_reward
         else:
             # create new state
-            next_state = main.create_state(game.snake[snake_number], done)
+            next_state = main.create_state(game.snake[snake_number], game.done[snake_number])
 
             # update memory
             DQNA.update_replay_memory(state,
                                       action,
                                       step_reward,
                                       next_state,
-                                      done
+                                      game.done[snake_number]
                                       )
 
         # reset steps and step limit
@@ -102,19 +104,17 @@ class main:
 
     # reset all for testing
     def reset(self):
-        # testing uses different number of games
         self.step = np.zeros([s_game_amount, 1])
         self.validation_reward = 0
 
         game.snake = np.zeros([s_game_amount, s_size[0] * s_size[1], 2])
         game.done = np.ones(s_game_amount, dtype=bool)
-        game.point = np.zeros(s_game_amount, dtype=bool)
 
         game.last_position = np.zeros([s_game_amount, 2])
         return
 
     # testing the AI with new games
-    def model_validation(self, e):
+    def model_validation(self):
         r_testing = True
         steps = 0
 
@@ -139,10 +139,7 @@ class main:
         # all games done
         cv2.destroyAllWindows()
 
-        # increase difficulty
-        main.increase_difficulty(steps, e)
-
-        return
+        return steps
 
     # increase difficulty based on test results
     def increase_difficulty(self, steps, e):
@@ -150,21 +147,19 @@ class main:
         step = steps / s_test_games
 
         # check limits
-        step_limit = main.step_limit * s_step_limit_multiplier
-        score_limit = main.step_limit * s_distance_score * s_score_limit_multiplier
+        step_limit = self.step_limit * s_step_limit_multiplier
+        score_limit = self.step_limit * s_distance_score * s_score_limit_multiplier
 
         # if steps and scores are good enough increase difficulty
         if step >= step_limit and avg >= score_limit and e != 1:
-            main.step_limit += s_step_increase
+            self.step_limit += s_step_increase
 
             # update graf limits
-            step_limit = main.step_limit * s_step_limit_multiplier
-            score_limit = main.step_limit * s_distance_score * s_score_limit_multiplier
+            step_limit = self.step_limit * s_step_limit_multiplier
+            score_limit = self.step_limit * s_distance_score * s_score_limit_multiplier
 
             # increase random point
-            game.random_poit += s_random_point_increase
-            if game.random_poit >= 1:
-                game.random_poit = 1
+            game.add_len += s_add_len
 
             # increase exploration
             DQNA.epsilon += s_epsilon_increase
@@ -176,21 +171,21 @@ class main:
         info.score_limit = score_limit
         info.avg_scores.append(avg)
         info.avg_step.append(step)
-        info.test_count.append(e)
+        info.episodes.append(e)
         info.last_step = step
         info.last_score = avg
 
         return
 
-
 if __name__ == '__main__':
     # initialize
     input_shape = np.zeros(s_state_size)
     DQNA = DQNAgent(input_shape)
-    info = info(tf)
-    main = main()
-    game = game()
+    info = INFO(tf)
+    main = MAIN()
+    game = GAME()
 
+    start = time.time()
     # define how many episodes (not games, all games with one step)
     for e in tqdm(range(1, s_episodes + 1), ascii=True, unit=' episodes'):
         r_testing = False
@@ -213,15 +208,26 @@ if __name__ == '__main__':
                 # reset values for testing
                 main.reset()
 
-                # test AI
-                main.model_validation(e)
+                # model validation
+                steps = main.model_validation()
+
+                # increase difficulty
+                main.increase_difficulty(steps, e)
 
                 # reset values for training
                 main.reset()
 
-                # draw graf
-                info.print_graf(DQNA.epsilon, e, game.random_poit)
+                # check graf lengths FIX THESE
+                info.balance_episodes()
+                info.balance_steps()
+                info.balance_scores()
 
-            # update graf values
-            elif e % 100 == 0:
-                info.print_graf(DQNA.epsilon, e, game.random_poit)
+                # draw graf
+                info.print_graf(DQNA.epsilon, e, game.add_len)
+
+        # funny timer
+        if time.time() - start >= 36_000:
+            print("Times up boy", time.time()-start)
+            quit()
+
+